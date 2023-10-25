e2bcaf4d6f185380c031366518da1f0df370c018
gdesmar <75089569+gdesmar@users.noreply.github.com>
2023-10-25 14:33:40 +0000

    Adding old urldownloader heuristics

--- urlcreator.py
+++ urlcreator.py
@@ -1,6 +1,36 @@
+import os
+import re
+from urllib.parse import urlparse
+
+from assemblyline.odm.base import IP_ONLY_REGEX, IPV4_ONLY_REGEX
 from assemblyline_v4_service.common.base import ServiceBase
 from assemblyline_v4_service.common.request import ServiceRequest
-from assemblyline_v4_service.common.result import Result, ResultTableSection, TableRow
+from assemblyline_v4_service.common.result import Heuristic, Result, ResultTableSection, TableRow
+
+# Threshold to trigger heuristic regarding high port usage in URI
+HIGH_PORT_MINIMUM = 1024
+
+# Common tools native to the platform that can be used for recon
+WINDOWS_TOOLS = ["dir", "hostname", "ipconfig", "netsh", "pwd", "route", "schtasks", "ver", "whoami"]
+LINUX_TOOLS = ["cat", "crontab", "hostname", "ip", "iptables", "ls", "pwd", "route", "uname", "whoami"]
+
+# Tools used for recon, sourced from https://github.com/ail-project/ail-framework/blob/master/bin/modules/Tools.py
+# fmt: off
+RECON_TOOLS = [
+    "amap", "arachni", "armitage", "arpscan", "automater", "backdoorfactory", "beef", "braa", "cdpsnarf", "cge",
+    "ciscotorch", "dig", "dirb", "dmytry", "dnmap", "dnscan", "dnsdict6", "dnsenum", "dnsmap", "dnsrecon", "dnstracer",
+    "dotdotpwn", "dsfs", "dsjs", "dsss", "dsxs", "enum4linux", "fierce", "fimap", "firewalk", "fragroute",
+    "fragrouter", "golismero", "goofile", "grabber", "hping3", "hydra", "identywaf", "intrace", "inurlbr", "ip",
+    "ismtp", "jbossautopwn", "john", "joomscan", "keimpx", "knock", "lbd", "ls", "maskprocessor", "masscan", "miranda",
+    "msfconsole", "ncat", "ncrack", "nessus", "netcat", "nikto", "nmap", "nslookup", "ohrwurm", "openvas", "oscanner",
+    "p0f", "patator", "phrasendrescher", "polenum", "rainbowcrack", "rcracki_mt", "reconng", "rhawk", "searchsploit",
+    "sfuzz", "sidguess", "skipfish", "smbmap", "sqlmap", "sqlninja", "sqlsus", "sslcaudit", "sslstrip", "sslyze",
+    "striker", "tcpdump", "theharvester", "uniscan", "unixprivesccheck", "wafw00f", "whatwaf", "whois", "wig", "wpscan",
+    "yersinia",
+]
+# fmt: on
+
+ALL_TOOLS = set(LINUX_TOOLS + WINDOWS_TOOLS + RECON_TOOLS)
 
 
 class URLCreator(ServiceBase):
@@ -17,13 +47,81 @@ class URLCreator(ServiceBase):
         # Only concerned with static/dynamic URIs found by prior services
         urls = tags.get("network.static.uri", []) + tags.get("network.dynamic.uri", [])
 
-        extracted_uri = ResultTableSection(title_text="Extracted URI")
+        scoring_uri = ResultTableSection(title_text="High scoring URI")
+        potential_ip_download = ResultTableSection(
+            title_text="Potential IP-related File Downloads", heuristic=Heuristic(1)
+        )
+        high_port_table = ResultTableSection("High Port Usage", heuristic=Heuristic(2))
+        tool_table = ResultTableSection("Discovery Tool Found in URI Path", heuristic=Heuristic(3))
 
         for tag_value, tag_score in urls:
-            if tag_score >= minimum_maliciousness:
-                request.add_extracted_uri(f"Score of {tag_score}", tag_value, request.get_uri_metadata(tag_value))
-                extracted_uri.add_row(TableRow(dict(URL=tag_value, Score=tag_score)))
-                extracted_uri.add_tag("network.static.uri", tag_value)
+            # Analyse the URL for the possibility of it being a something we should download
+            parsed_url = urlparse(tag_value)
+            interesting_features = []
 
-        if extracted_uri.body:
-            request.result.add_section(extracted_uri)
+            if tag_score >= minimum_maliciousness:
+                scoring_uri.add_row(TableRow(dict(URL=tag_value, Score=tag_score)))
+                scoring_uri.add_tag("network.static.uri", tag_value)
+                interesting_features.append(f"Score of {tag_score}")
+
+            if re.match(IP_ONLY_REGEX, parsed_url.hostname) and "." in os.path.basename(parsed_url.path):
+                # If URL host is an IP and the path suggests it's downloading a file, it warrants attention
+                ip_version = "4" if re.match(IPV4_ONLY_REGEX, parsed_url.hostname) else "6"
+                potential_ip_download.add_row(
+                    TableRow(
+                        {
+                            "URL": tag_value,
+                            "HOSTNAME": parsed_url.hostname,
+                            "IP_VERSION": ip_version,
+                            "PATH": parsed_url.path,
+                        }
+                    )
+                )
+                potential_ip_download.add_tag("network.static.uri", tag_value)
+                potential_ip_download.add_tag("network.static.ip", parsed_url.hostname)
+                if not potential_ip_download.heuristic:
+                    potential_ip_download.set_heuristic(3)
+                potential_ip_download.heuristic.add_signature_id(f"ipv{ip_version}")
+                interesting_features.append("IP as hostname")
+
+            if parsed_url.port and parsed_url.port > HIGH_PORT_MINIMUM:
+                # High port usage associated to host
+                high_port_table.heuristic.add_signature_id(
+                    "ip" if re.match(IP_ONLY_REGEX, parsed_url.hostname) else "domain"
+                )
+                high_port_table.add_row(
+                    TableRow({"URI": tag_value, "HOST": parsed_url.hostname, "PORT": parsed_url.port})
+                )
+                interesting_features.append("High port")
+
+            # Check if URI path is greater than the smallest tool we can look for (ie. '/ls')
+            if parsed_url.path and len(parsed_url.path) > 2:
+                path_split = parsed_url.path.lower().split("/")
+                for tool in ALL_TOOLS:
+                    if tool in path_split:
+                        # Native OS tool found in URI path
+                        if tool in LINUX_TOOLS:
+                            tool_table.heuristic.add_signature_id("linux")
+                        if tool in WINDOWS_TOOLS:
+                            tool_table.heuristic.add_signature_id("windows")
+                        if tool in RECON_TOOLS:
+                            tool_table.heuristic.add_signature_id("recon")
+
+                        tool_table.add_row(TableRow({"URI": tag_value, "HOST": parsed_url.hostname, "TOOL": tool}))
+                        interesting_features.append("Tool in path")
+
+            if interesting_features:
+                request.add_extracted_uri(
+                    f"Feature{'s' if len(interesting_features)>1 else ''}: {', '.join(interesting_features)}",
+                    tag_value,
+                    request.get_uri_metadata(tag_value),
+                )
+
+        if scoring_uri.body:
+            request.result.add_section(scoring_uri)
+        if potential_ip_download.body:
+            request.result.add_section(potential_ip_download)
+        if high_port_table.body:
+            request.result.add_section(high_port_table)
+        if tool_table.body:
+            request.result.add_section(tool_table)
